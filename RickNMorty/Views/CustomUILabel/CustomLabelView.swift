48a4dba92b80df9a6212b7e3ae9d91397102096f
dabid <abd5@umbc.edu>
2023-05-15 18:15:49 -0400

    adding custom ui Label

---
+++ RickNMorty/Views/CustomUILabel/CustomLabelView.swift
@@ -0,0 +1,14 @@
+//
+//  CustomLabelView.swift
+//  RickNMorty
+//
+//  Created by Dan Abid on 5/15/23.
+//
+
+import Foundation
+import UIKit
+class CustomLabelView: UIView{
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var gradientView: GradientBorderShadowView!
+}
