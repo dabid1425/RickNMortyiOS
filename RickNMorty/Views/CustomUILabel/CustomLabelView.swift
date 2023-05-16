65d0a30981d4b95255c105ed63e5d87bc0419c5e
dabid <abd5@umbc.edu>
2023-05-16 09:56:44 -0400

    messing around with images inside custom view

--- RickNMorty/Views/CustomUILabel/CustomLabelView.swift
+++ RickNMorty/Views/CustomUILabel/CustomLabelView.swift
@@ -9,6 +9,9 @@ import Foundation
 import UIKit
 class CustomLabelView: UIView{
     
+    @IBOutlet var rightImage: UIView!
+    @IBOutlet var leftImage: UIView!
+    @IBOutlet var horizontalStackView: UIStackView!
     @IBOutlet var label: UILabel!
     @IBOutlet var gradientView: GradientBorderShadowView!
 }
