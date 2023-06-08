87b9a14a38877ecfa5a895a0a55eaeb30c78d31d
dabid <abd5@umbc.edu>
2023-06-08 11:24:07 -0400

    adding init

--- RickNMorty/Views/CustomUILabel/CustomLabelView.swift
+++ RickNMorty/Views/CustomUILabel/CustomLabelView.swift
@@ -19,6 +19,24 @@ class CustomLabelView: UIView{
               bindViewModel()
           }
       }
+    override init(frame: CGRect) {
+           super.init(frame: frame)
+           setupView()
+       }
+       
+       required init?(coder aDecoder: NSCoder) {
+           super.init(coder: aDecoder)
+           setupView()
+       }
+       
+       private func setupView() {
+           let bundle = Bundle(for: type(of: self))
+           let nib = UINib(nibName: "TextFieldView", bundle: bundle)
+           let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+           addSubview(view)
+           view.frame = bounds
+           view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+       }
     private func bindViewModel() {
         guard let viewModel = viewModel else { return }
         
